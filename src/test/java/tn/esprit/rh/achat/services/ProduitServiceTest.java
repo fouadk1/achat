86bdd4cfa399755fded32126f5935b8815a11e1c
fouadk1 <fouedkanzaoui@gmail.com>
2023-01-10 20:40:16 +0100

    add test retreive produit

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -3,24 +3,21 @@ package tn.esprit.rh.achat.services;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.rh.achat.entities.Produit;
-import tn.esprit.rh.achat.services.IProduitService;
 
+import tn.esprit.rh.achat.entities.Produit;
 import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class ProduitServiceTest {
+class ProduitServiceTest {
 
-    @Autowired
-    IProduitService ps;
+	@Autowired
+	private IProduitService ps;
 
-    @Test
-    @Order(1)
-    public void testRetrieveAllUsers() {
-        List<Produit> listProduits = ps.retrieveAllProduits();
-        Assertions.assertEquals(0, listProduits.size());
-    }
-
+	@Test
+	public void shouldReturnAllProduit() {
+		List<Produit> expected = ps.retrieveAllProduits();
+		Assertions.assertEquals(expected.size(), 0);
+	}
 
 }
