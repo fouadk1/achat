6d82719c4321be2b0ec7db7a96c550c8ee99e6ef
AmaniAvaxia <amani.sahli@avaxia-group.com>
2023-01-10 15:53:38 +0100

    new Test code

---
+++ src/test/java/tn/esprit/rh/achat/services/ReglementServiceTest.java
@@ -0,0 +1,36 @@
+package tn.esprit.rh.achat.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Reglement;
+import tn.esprit.rh.achat.repositories.ReglementRepository;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class ReglementServiceTest {
+ @Autowired
+ IReglementService rs;
+ @Test
+ @Order(1)
+ public void testRetrieveAllReglements() {
+ 
+System.out.println("This is the testcase in this class:Reglements"); 
+List<Reglement> listreglements = rs.retrieveAllReglements();
+ Assertions.assertEquals(0, listreglements.size());
+ }
+}
