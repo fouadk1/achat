385e1fd05e27247f6d3b8c68b7520d14f9bb12b9
Sofiene Mabrouk <sofienembk@gmail.com>
2023-01-12 17:04:38 +0100

    changes test

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -9,6 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AchatApplication {
 
     public static void main(String[] args) {
+         String MYSQL_HOST = System.getenv("MYSQL_HOST");
+        System.out.println(MYSQL_HOST);
         SpringApplication.run(AchatApplication.class, args);
     }
 
